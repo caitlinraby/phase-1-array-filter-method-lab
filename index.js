5d9478e577defe7f06c9b385daf98a3bcdefb3e3
Caitlin Raby <caitlinraby@gmail.com>
2021-04-16 15:58:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+
+function findMatching (collection, name) { 
+  return collection.filter(function (user) {
+    return user.toLowerCase() === name.toLowerCase()});
+}
+
+
+
+function fuzzyMatch (collection, string) {
+  return collection.filter(function (startsWith) {
+    return startsWith[0] === string[0]});
+}
+
+function matchName (collection, string) {
+  return collection.filter(function(element) {
+    return element.name === string});
+  }
